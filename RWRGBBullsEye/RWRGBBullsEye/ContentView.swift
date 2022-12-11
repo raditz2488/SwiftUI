2920fb6f0acdf93b539886b808eb6b200ee8ea13
robh <rohanbhale@Rohans-MacBook-Pro.local>
2022-12-11 13:31:14 +0530

    [RWRGBBullsEye] - Created project

---
+++ RWRGBBullsEye/RWRGBBullsEye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RWRGBBullsEye
+//
+//  Created by Rohan Bhale on 11/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RWRGBBullsEye/RWRGBBullsEye/RWRGBBullsEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RWRGBBullsEyeApp.swift
+//  RWRGBBullsEye
+//
+//  Created by Rohan Bhale on 11/12/22.
+//
+
+import SwiftUI
+
+@main
+struct RWRGBBullsEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
